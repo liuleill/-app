3679bd88567c85bbbfeff086d75566ef85d70829
刘磊 <2413882532@qq.com>
2021-05-18 18:37:01 +0800

    引入全局组件<Nav/>

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
         Labels.vue
+        <Nav/>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name:'Labels',
     }
 </script>
 
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,13 @@
 <template>
     <div>
         Statistics.vue
+        <Nav/>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name:'Statistics',
     }
 </script>
 
