5900b6d6348d7788cd27ab010f624ae1bb0d248c
刘磊 <2413882532@qq.com>
2021-05-18 20:18:15 +0800

    money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        Money.vue
+    <div class="nav-wrapper">
+        <div class="content">
+            Money.vue
+        
+        </div>
         <Nav/>
     </div>
 </template>
@@ -12,5 +15,14 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .nav-wrapper{
+        border:1px solid green;
+        display: flex;
+        flex-direction: column;
+        height:100vh;
+    }
+    .content{
+        flex-grow:1;
+        overflow: auto;
+    }
 </style>
