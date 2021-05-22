85fb4a1c7e9ac92a553e4089bc636d96d8fbb614
刘磊 <2413882532@qq.com>
2021-05-22 20:35:24 +0800

    添加全局样式和全局变量

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,10 +1,48 @@
 <template>
-    <div>
-        <Layout>
-            <p>money</p>
-        </Layout>
-    </div>
-    
+    <Layout>
+        <div class="tags">
+            <ul class="current">
+                <li>衣</li>
+                <li>食</li>
+                <li>住</li>
+                <li>行</li>
+            </ul>
+            <div class="new">
+                <button>新增标签</button>
+            </div>
+        </div>
+        <div>
+            <label class="notes">
+                <span>备注</span>
+                <input type="text">
+            </label>
+        </div>
+        <div>
+            <ul class="types">
+                <li class="selected">支出</li>
+                <li>收入</li>
+            </ul>
+        </div>
+        <div>
+            <div>100</div>
+            <div>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>delete</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>clear</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>OK</button>
+                <button>0</button>
+                <button>.</button>
+            </div>
+        </div>
+    </Layout>
 </template>
 
 <script lang="ts">
@@ -14,5 +52,5 @@
 </script>
 
 <style lang="scss" scoped>
-
+@import "~@/assets/style/helper.scss";
 </style>
