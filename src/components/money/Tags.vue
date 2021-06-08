92f5a81852f9ddc4f19ccd8197887c801b283cb1
刘磊 <2413882532@qq.com>
2021-06-08 17:34:19 +0800

    Tags.vue使用Vuex

--- src/components/money/Tags.vue
+++ src/components/money/Tags.vue
@@ -20,13 +20,17 @@ import {Component,Prop} from 'vue-property-decorator';
 @Component({
   computed:{
     tagList(){
-      return []
+      return this.$store.state.tagList;
     }
   }
 })
   export default class Tags extends Vue{
     selectedTags:string[] = [];
     
+    created(){
+      this.$store.commit('fetchTags');
+    }
+
     toggle(tag:string){
       const index = this.selectedTags.indexOf(tag);
       if(index >=0 ){
@@ -39,6 +43,7 @@ import {Component,Prop} from 'vue-property-decorator';
     create(){
       const name = window.prompt('请输入标签名:');
       if(!name) {return window.alert('标签名不能为空');}
+      this.$store.commit('createTag',name);
     }
   };
 </script>
