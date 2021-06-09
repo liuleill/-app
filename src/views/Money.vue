8f1a64a498bccfd193c9c19a8b3c6e4e0ff48a21
刘磊 <2413882532@qq.com>
2021-06-09 20:28:05 +0800

    在Money.vue里面使用Tabs

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,74 +1,71 @@
 <template>
-    <Layout class-prefix="layout">
-        {{record}}
-        <NumberPad :value.sync="record.amount" @submit="saveRecord"/>        
-        <Types :value.sync="record.type"/>
-        <div class="notes">
-            <FormItem  fieldName="备注"
-                    placeholder="在这里输入备注"
-                    @update:value="onUpdateNotes"
-            />
-        </div>
-        <Tags/>
-    </Layout>
+  <Layout class-prefix="layout">
+    {{ record }}
+    <NumberPad :value.sync="record.amount" @submit="saveRecord" />
+    <Types :data-source="recordTypeList" :value.sync="record.type" />
+    <div class="notes">
+      <FormItem
+        fieldName="备注"
+        placeholder="在这里输入备注"
+        @update:value="onUpdateNotes"
+      />
+    </div>
+    <Tags />
+  </Layout>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue';
-    import NumberPad from '@/components/money/NumberPad.vue';
-    import Types from '@/components/money/Types.vue';
-    import FormItem from '@/components/money/FormItem.vue';
-    import Tags from '@/components/money/Tags.vue';
-    import {Component} from 'vue-property-decorator';
+import Vue from "vue";
+import NumberPad from "@/components/money/NumberPad.vue";
+import Types from "@/components/money/Types.vue";
+import FormItem from "@/components/money/FormItem.vue";
+import Tags from "@/components/money/Tags.vue";
+import { Component } from "vue-property-decorator";
+import Tabs from "@/components/Tabs.vue";
+import recordTypeList from "@/constants/recordTypeList.ts";
 
-    @Component({
-        components:{Tags,FormItem,Types,NumberPad},
-        computed:{
-            recordList(){
-                return this.$store.state.recordList;
-            }
-        }
-    })
-    export default class Money extends Vue {
-        get recordList(){
-            return this.$store.state.recordList;
-        };
+@Component({
+  components: { Tabs, Tags, FormItem, Types, NumberPad },
+  computed: {
+    recordList() {
+      return this.$store.state.recordList;
+    },
+  },
+})
+export default class Money extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
 
-        record:RecordItem = {
-            tags:[],notes:'',type:'-',amount:0
-        };
+  recordTypeList = recordTypeList;
 
-        created(){
-            this.$store.commit('fetchRecords');
-        }
+  record: RecordItem = {
+    tags: [],
+    notes: "",
+    type: "-",
+    amount: 0,
+  };
 
-        // onUpdateTags(value:string[]){
-        //     this.record.tags = value;
-        // }
-        onUpdateNotes(value:string){
-            this.record.notes = value;
-        }
-        
-        // onUpdateType(value:string){
-        //     this.record.type = value;
-        // }
+  created() {
+    this.$store.commit("fetchRecords");
+  }
 
-        // onUpdateAmount(value:string){
-        //     this.record.amount = parseFloat(value);
-        // }
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
+  }
 
-        saveRecord(){
-            this.$store.commit('createRecord',this.record);
-        }
-    };
+  saveRecord() {
+    this.$store.commit("createRecord", this.record);
+  }
+}
 </script>
 
 <style lang="scss">
-    .layout-content{
-        display: flex;
-        flex-direction: column-reverse;
-    }
-    .notes {
-        padding: 12px 0;
-    }
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
+.notes {
+  padding: 12px 0;
+}
 </style>
