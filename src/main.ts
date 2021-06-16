e1cebcb8ddbd9f74e495c28930c83c0225ab730b
刘磊 <2413882532@qq.com>
2021-06-16 20:25:36 +0800

    保证用户可以用手机打开页面

--- src/main.ts
+++ src/main.ts
@@ -26,3 +26,15 @@ window.onload = function(){
         window.scrollTo(0,10000);
   },0);
 };
+
+if(document.documentElement.clientWidth > 500){
+  //window.alert('为了浏览效果，请使用手机查看');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.style.position='fixed';
+  img.style.left = '50%';
+  img.style.top = '50%';
+  img.style.transform = 'translate(-50%,-50%)'; 
+  img.style.boxShadow = '0 0 10px rgba(0,0,0)';
+  document.body.appendChild(img);
+}
