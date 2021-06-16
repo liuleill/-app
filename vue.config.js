1a3e38c17f0a4feb902920dfa2165df235fe7f25
刘磊 <2413882532@qq.com>
2021-06-16 18:03:21 +0800

    修复8个commit的报错

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@
 const path = require('path');
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/money-show/'
+    : '/',
   lintOnSave: false,
   lintOnSave: false,
   chainWebpack: config =>{
